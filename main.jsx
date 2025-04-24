ac585399e7a04f32482be0a77bff911f7755d2bd
GarnrEdge <aidanmakk@outlook.com>
2025-04-24 17:04:45 +1000

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import App from "./App.jsx";
-import Overview from "./pages/Overview.jsx";
-import Analytics from "./pages/Analytics.jsx";
+import Overview from "./Overview.jsx";
+import Analytics from "./Analytics.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
